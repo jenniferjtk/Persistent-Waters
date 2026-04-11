9c748b7ba2207d626c3883d4686549ddde17e77f
jenniferjtk <jennagracejohnson3@gmail.com>
2026-04-11 12:45:28 -0400

    allow CORS

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, X-Test-Password');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 require_once 'helpers/response.php';
 
 $method = $_SERVER['REQUEST_METHOD'];
